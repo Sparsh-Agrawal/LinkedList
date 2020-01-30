5eab72405a93ba1e57b05a5a3e8ee9cffb1653ef
Sparsh Agrawal <sparsh.agrawal_ccv18@gla.ac.in>
2020-01-30 16:12:44 +0530

    LinkedList

--- singlylinkedlist/implementation/MyLinkedListImplementation.java
+++ singlylinkedlist/implementation/MyLinkedListImplementation.java
@@ -1,6 +1,13 @@
 package singlylinkedlist.implementation;
 
+import singlylinkedlist.linkedlist.MyLinkedList;
+
 public class MyLinkedListImplementation
 {
-
+    public static void main(String[] args)
+    {
+        MyLinkedList linkedList=new MyLinkedList();
+        linkedList.insertAtLast(20);
+        linkedList.insertAtLast(10);
+    }
 }
